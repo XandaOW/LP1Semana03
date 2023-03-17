437db2a1d7c3252b3f95fce26edac4ba1876baf8
Vasco Caleiro <a22203051@alunos.ulht.pt>
2023-03-17 15:07:12 +0000

    exercicio 1

---
+++ ChangeString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ChangeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nplease insert a string: ");
+            string str = Console.ReadLine();
+
+            Console.Write("please insert a char to be removed from the string: ");
+            char charToBeRemoved = char.Parse(Console.ReadLine());
+            
+            string newString = "";
+            foreach(char character in str) {
+                if (character == charToBeRemoved) continue;
+                newString += character;
+            }
+        
+            Console.WriteLine($"\nthis is you treated string with all the {charToBeRemoved} removed: {newString}");
+        }
+    }
+}
